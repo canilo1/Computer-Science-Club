5ad52e11aa3e2e35418f78236c15bf019fd7c401
John Gutierrez <75353216+canilo1@users.noreply.github.com>
2025-01-21 01:04:47 -0800

    Laid out some foundation, started working on the HomePage

---
+++ comsciclub/src/app/SideBar.js
@@ -0,0 +1,17 @@
+const SideBar = () => {
+return(<>
+<section id = 'whitebackground' className="sidebar" >    
+</section>
+<section className="sidebar"> 
+    <h1 id = "LMCCS" className="sidebar">LMCCS</h1>
+    <img id = "LMCCSID"  className = "sidebar"src="https://i.imgur.com/1Z2Q2Zz.png" alt="LMCCS Logo"/>
+    <div className = "links">
+        <a  id = "HomeLink" href = "#">Home</a>
+        <a  id = "EventsLink" href = "#">Events</a>
+        <a id = "ContactLink" href = "#">Contact</a>
+    </div>
+</section>
+
+</>);
+
+};
