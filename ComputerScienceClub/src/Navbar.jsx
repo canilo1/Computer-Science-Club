898a80de886d041338489cae8a340c6e2fdcbc2a
John Gutierrez <75353216+canilo1@users.noreply.github.com>
2025-01-22 19:31:12 -0800

    Moved to vite, next was too complicated

---
+++ ComputerScienceClub/src/Navbar.jsx
@@ -0,0 +1,17 @@
+//import { useState } from "react";
+
+const Navbar = () => {
+    return (
+        <nav id = "Navbar"> 
+           <div id = "LMCCSContainer">
+                <a className="LMCCS" id = "LMCCS">LMCCS</a>
+            </div>
+            <div className="DirectoryLinks">
+                <a href="/" id = "Home">Home</a>
+                <a href="/about" id = "About">About</a>
+                <a href="/events" id = "Events">Events</a>
+            </div>
+        </nav>
+    );
+};
+export default Navbar;
