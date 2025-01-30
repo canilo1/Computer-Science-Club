d8951e28ef5d73ecd1279d597fe752b309059e04
John Gutierrez <75353216+canilo1@users.noreply.github.com>
2025-01-29 16:18:37 -0800

    Pushing this through

--- ComputerScienceClub/src/Events.jsx
+++ ComputerScienceClub/src/Events.jsx
@@ -6,13 +6,20 @@ const Events = () => {
         <section className = "UpComingEvents">
             <h2 id = "UpcomingEventsTitle">Upcoming Events</h2>
             <section className = "UpComingEventsContainer">
-
+            <section id = "Event1UpComing" className = "Upcomingevent">
+              </section>
+              <section id = "Event2UpComing" className = "Upcomingevent"></section>
+              <section id = "Event3UpComing" className = "Upcomingevent"></section>
+              <section id  = "Event4UpComing" className = "Upcomingevent"></section>
             </section>
         </section>
         <section className = "PastEvents">
             <h2>Past Events</h2>
             <section className="PastEventsContainer">
-
+              <section id = "Event1UpComing" className = "PastEvent"></section>
+              <section id = "Event2UpComing"className = "PastEvent"></section>
+              <section id = "Event3UpComing"className = "PastEvent"></section>
+              <section id  = "Event4UpComing"className = "PastEvent"></section>
             </section>
             
         </section>
